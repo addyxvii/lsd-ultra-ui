1ae32b57b6634c5e7ae78bc783eed13421f4af50
addyxvii <addisonbennett@gmail.com>
2023-03-04 11:26:32 -0700

    tailwind

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,13 +1,29 @@
-import { useAccount, useEnsName } from 'wagmi'
+import { useAccount, useEnsName } from "wagmi";
+import { configureChains, createClient } from "wagmi";
+import { mainnet, optimism } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+const { chains, provider } = configureChains(
+  [mainnet, optimism],
+  [alchemyProvider({ apiKey: "yourAlchemyApiKey" }), publicProvider()]
+);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: [new InjectedConnector({ chains })],
+  provider,
+});
 
 export function Account() {
-  const { address } = useAccount()
-  const { data: ensName } = useEnsName({ address })
+  const { address } = useAccount();
+  const { data: ensName } = useEnsName({ address });
 
   return (
     <p>
       {ensName ?? address}
       {ensName ? ` (${address})` : null}
     </p>
-  )
+  );
 }
